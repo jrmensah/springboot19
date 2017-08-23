0f465b79d073ce996c0960a23f0a90e5b2d37ee5
jrmensah <jmensah4@student.umuc.edu>
2017-08-23 15:41:54 -0400

    First commit-Security-Specific Permissions

---
+++ src/main/java/me/jerilynmensah/springboot19/Springboot19Application.java
@@ -0,0 +1,12 @@
+package me.jerilynmensah.springboot19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot19Application.class, args);
+	}
+}
